29026df6ab059a2e9782d3c109b7de521f375c5b
Simon Charette <charette.s@gmail.com>
2015-01-21 00:10:39 -0500

    Added setup classifier and extra requirements.

--- setup.py
+++ setup.py
@@ -18,6 +18,28 @@ setup(
     license='MIT',
     packages=find_packages('sundial'),
     install_requires=requirements,
+    extras_require={
+        ['south']: ['south']
+    },
     classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )

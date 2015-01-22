993da584ba0a939c44d84695a8b13abfe2a02a59
Simon Charette <charette.s@gmail.com>
2015-01-22 16:21:58 -0500

    Added PEP 386-compliant __version__.

--- sundial/__init__.py
+++ sundial/__init__.py
@@ -1,6 +1,10 @@
 from __future__ import unicode_literals
 
+from sundial.versioning import get_version
+
+
 __all__ = ['VERSION', '__version__']
 
 VERSION = (1, 0, 0, 'alpha', 0)
-__version__ = '.'.join(str(v) for v in VERSION)
+
+__version__ = get_version(VERSION)

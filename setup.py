3dc509ea07f7919d17cbc1f3dd6a69c68dd37ed8
Simon Charette <charette.s@gmail.com>
2017-09-12 23:26:08 -0400

    Officialize support for Python 3.6.

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Internet :: WWW/HTTP :: WSGI',

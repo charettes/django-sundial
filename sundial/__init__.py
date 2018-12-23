85e2715c247267ce54aadc1dc29be4273eea9f69
Simon Charette <charette.s@gmail.com>
2018-12-23 02:08:01 -0500

    Bumped version number to 1.0.6. Somehow Pypi won't allow 1.0.5 uploads anymore.

--- sundial/__init__.py
+++ sundial/__init__.py
@@ -5,6 +5,6 @@ from django.utils import version
 
 __all__ = ['VERSION', '__version__']
 
-VERSION = (1, 0, 6, 'alpha', 0)
+VERSION = (1, 0, 6, 'final', 0)
 
 __version__ = version.get_version(VERSION)

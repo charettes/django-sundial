b9d5838f51820d89e107d7247f76327fac01a80c
Simon Charette <charette.s@gmail.com>
2020-05-09 01:59:16 -0400

    Bumped version to 1.1.0.

--- sundial/__init__.py
+++ sundial/__init__.py
@@ -2,6 +2,6 @@ from django.utils import version
 
 __all__ = ['VERSION', '__version__']
 
-VERSION = (1, 0, 7, 'alpha', 0)
+VERSION = (1, 1, 0, 'final', 0)
 
 __version__ = version.get_version(VERSION)

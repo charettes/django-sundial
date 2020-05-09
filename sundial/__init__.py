a552151cdeefb718def8f29eb82ee940e15f12cf
Simon Charette <charette.s@gmail.com>
2020-05-09 01:59:42 -0400

    Started 1.1.1 alpha development.

--- sundial/__init__.py
+++ sundial/__init__.py
@@ -2,6 +2,6 @@ from django.utils import version
 
 __all__ = ['VERSION', '__version__']
 
-VERSION = (1, 1, 0, 'final', 0)
+VERSION = (1, 1, 1, 'alpha', 0)
 
 __version__ = version.get_version(VERSION)
